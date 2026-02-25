82e5c9df79cdcad924289906d96ab590942ba029
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-02-26 00:12:52 +0600

    Created form and add and remove functionality

--- src/components/RecipeApp.tsx
+++ src/components/RecipeApp.tsx
@@ -14,6 +14,27 @@ export const RecipeApp = () => {
     const [ingredients, setIngredients] = useState<string>("");
     const [instructions, setInstructions] = useState<string>("");
     const [editingRecipe, setEditingRecipe] = useState<Recipe | null>(null);
+
+const handleAddRecipe=()=>{
+    if(name.trim()==="" || ingredients.trim()==="" || instructions.trim()===""){
+        return;
+    }
+    addRecipe({
+        id:Date.now(),
+        name,
+        ingredients:ingredients.split(",").map(ingredients=>ingredients.trim()),
+        instructions,
+    });
+
+    setName("");
+    setIngredients("");
+    setInstructions("")
+
+}
+
+
+
+
     return (
         <div className='min-h-screen flex items-center justify-center bg-green-100'>
             <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl">
@@ -23,18 +44,53 @@ export const RecipeApp = () => {
                 <input type="text" value={name}
                     onChange={(e) => setName(e.target.value)}
                     placeholder='Recipe Name'
-                    className='w-full px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                    className='w-full px-4 py-2 mb-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
                 />
                 <input type="text" value={ingredients}
                     onChange={(e) => setIngredients(e.target.value)}
                     placeholder='Ingredients(Comma Separated)'
-                    className='w-full px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                    className='w-full px-4 py-2 mb-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
                 />
                 <textarea value={instructions}
                     onChange={(e) => setInstructions(e.target.value)}
                     placeholder='Instructions'
-                    className='w-full px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                    className='w-full px-4 py-2 mb-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500'
                 />
+
+                <div className='flex justify-between'>
+                {
+                    editingRecipe?
+                    (<>
+                        <button onClick={handleUpdateRecipe} className='bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:border-yellow-500'>Update Recipe</button>
+                        <button onClick={handleCancelEdit} className='bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:border-gray-500'>Cancel</button>
+                        </>)
+                    :
+                    <>
+                    <button onClick={handleAddRecipe} className='bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:border-gray-500'>Add Recipe</button>
+                    </>
+                }
+                </div>
+                <ul className='space-y-4'>
+                    {recipes.map((recipe)=>(
+                        <li key={recipe.id} className='p-4 bg-green-50 rounded-lg shadow-sm'>
+                            <h2 className='text-xl font-semibold  text-green-800 mb-2'>
+                                {recipe.name}
+                            </h2>
+
+                            <p className='text-green-700 mb-2'>
+                                <strong>Ingredients:</strong>{recipe.ingredients.join(", ")}
+                            </p>
+                            <div className='flex justify-between'>
+                                <button onClick={()=>handleEditRecipe(recipe)}
+                                    className='bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:border-yellow-500'>Edit</button>
+                                <button onClick={()=>removeRecipe(recipe.id)}
+                                    className='bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:border-red-500'>Delete</button>
+                            </div>
+                        </li>
+                    )
+                    
+                    )}
+                </ul>
             </div>
         </div>
     )
